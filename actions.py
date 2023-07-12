35bcdd21e17ae024976188bfcbe8b7ee635f497d
RenosGPS <gpszat@gmail.com>
2023-07-12 16:18:17 +0300

    Update actions.py

--- actions.py
+++ actions.py
@@ -2,3 +2,15 @@
 # ACTIONS MODULE
 # ---------------------------------
 
+
+def add(a,b):
+  print(a + b)
+
+def minus(a,b):
+  print(a - b)
+
+def mult(a,b):
+  print(a*b)
+
+def div(a,b):
+  print(a/b)
